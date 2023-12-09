c221311be0211c2f446cc3a1a1c2a6fd1f51aea6
Moe Odeh <m.odeh93@gmail.com>
2023-12-09 15:20:47 -0500

    Seperate MIMO_Independent_SISO from MIMO

--- LinearTurbine/MIMO/simulinkModels/FMU_QFT_MIMO_Regime3_Simulink_Specific/sources/fmiModelIdentifier.h
+++
@@ -1,5 +0,0 @@
-#ifndef FMI_MODELIDENTIFIER_H 
-#define FMI_MODELIDENTIFIER_H 
-#define MODEL_IDENTIFIER FMU_QFT_MIMO_Regime3_Simulink_Specific 
-#define FMI_MODULE_NAME FMU_QFT_MIMO_Regime3_Simulink_Specific.dll 
-#endif 
